dc25260ea1d4a6ba5c6b5d4132ba753950cd714b
Parallels <morrisyau@college.harvard.edu>
2016-07-07 21:32:36 -0400

    first commit

--- Newton2.m
+++ Newton2.m
@@ -1,35 +1,117 @@
 
-function [x,i,lambda, f, dualf, dualArg] = Newton2(A, b, c, T, x0, maxIter, threshold, alpha, beta)
+function [x,i,lambda, f, dualf, dualArg] = Newton2(Q, A, b, c, T, x0, maxIter, threshold, alpha, beta, quadratic_tail, penetrate)
 %compute initial parameters
 [m,n] = size(A);
 x = zeros(n, maxIter);
 x(:,1) = x0;
+dx = [];
+hess = [];
+g = [];
+
+q = -10; %Parameter set in paper  
+s0 = -1/q;
+C = [[2*s0, 1, 0];[2, 0, 0];[0, 0, 1]];
+r = [-1/s0;1/(s0^2);-log(s0)];
+a = C\r;
 
 %begin iteration loop
 for i = 1:maxIter
     %gradient and hessian
-    f = T*c'*x(:,i) - sum(log(b-A*x(:,i)));
-    d = 1./(b - A * x(:,i));
-    grad = T*c + A' * d;
-    Hess = A' * diag(d.^2) * A;
+    %f = T*c'*x(:,i) - sum(log(b-A*x(:,i)));
+    if A*x(:,i) >= b
+        quadratic_tail = 0
+    end
+    
+    if quadratic_tail
+        A_pen = A(1:penetrate,:);
+        s_pen = A_pen*x(:,i);
+        quad_log = s_pen >= s0*ones(penetrate,1);
+        quad_log = [quad_log; ones(m-penetrate,1)];
+        A_feasible = zeros(m,n);
+        A_unfeasible = zeros(m,n);
+        A_feasible(find(quad_log),:) = A(find(quad_log),:)
+        A_unfeasible(find(~quad_log),:) = A(find(~quad_log),:);
+        b_feasible = zeros(m,1);
+        b_unfeasible = zeros(m,1);
+        b_feasible(find(quad_log)) = b(find(quad_log));
+        b_feasible(find(~quad_log)) = b(find(~quad_log));
+        feasible = sum(log(A_feasible * x(:,i)));
+        unfeasible = sum(log(A_unfeasible*x(:,i)));
+        f = T*x(:,i)'*Q*x(:,i) + T*c'*x(:,i) - feasible - unfeasible;
+        d_feasible = 1./(A_feasible*(x:,i) - b_feasible)
+        grad = T*diag(diag(Q))*x(:,i) + T*Q*x(:,i) + T*c - A_feasible'*d_feasible + ones(m,1)'*2*a(1)*diag(A_unfeasible*x(:,i)*A_unfeasible + a(2)*A_unfeasible);
+        Hess = T*diag(diag(Q)) + T*Q + A_feasible' *diag(d_feasible.^2)*A_feasible + 2*a(1)*(A_unfeasible'*A_unfeasible)  
+    else 
+        f = T*x(:,i)'*Q*x(:,i) + T*c'*x(:,i) - sum(log(A*x(:,i) - b));
+        d = 1./(A*x(:,i) - b);
+        grad = T*diag(diag(Q))*x(:,i) + T*Q*x(:,i) + T*c - A'*d;
+        Hess = T*diag(diag(Q)) + T*Q + A' * diag(d.^2) * A;
+    end
+    %disp('debug variables')
+    %disp(T);
+    %disp(c);
+    %disp(x);
+    %disp(T*c'*x(:,i));
+    %disp(sum(log(b-A*x(:,i))));
+    %disp(f);
+ 
+    
+    %d = 1./(b - A * x(:,i));
+    %grad = T*c + A' * d;
+    %Hess = A' * diag(d.^2) * A;
+    
+    %d = 1./(A*x(:,i) - b);
+    %grad = T*diag(diag(Q))*x(:,i) + T*Q*x(:,i) + T*c - A'*d;
+    %Hess = T*diag(diag(Q)) + T*Q + A' * diag(d.^2) * A;
+    hess = [hess Hess];
+    g = [g grad];
+    
     
     %Newton step and decrement
     Dx = - Hess \ grad;
     lambda = - grad' * Dx;
+    dx = [dx Dx];
     
     %stopping criterion
-    if (lambda / 2) < threshold
+    if (lambda / 2) < threshold || i == maxIter || ~isempty(find(isnan(Dx),1)) || ~isempty(find(isinf(Dx),1))
+        if (lambda / 2) < threshold
+            disp('threshold exit')
+        elseif i == maxIter
+            disp('iteration exit')
+        elseif  ~isempty(find(isnan(Hess),1)) || ~isempty(find(isinf(Hess),1))
+            disp('boundary collision exit')
+        end
         dualf = f -m/T;
         dualArg = 1/T * d;
         break
     else
         %backtracking line search
+        bound = b - A*x(:,i);
         t = 1;
-        while T*c'*(x(:,i)+t*Dx) -sum(log(b-A*(x(:,i)+ t*Dx))) > (f+alpha*t*grad'*Dx)
-            t = beta * t ;
+        %arbitrary_stepsize = 1;
+        adx = A*Dx;
+        for j = 1:size(bound,1)
+            if ~isnan(bound(j)/adx(j)) && ~isinf(bound(j)/adx(j))
+                if bound(j)/adx(j) > 0
+%                     if arbitrary_stepsize == 1
+%                         t = min(t,bound(j)/adx(j));
+%                         arbitrary_stepsize = 0;
+%                     else
+%                         t = min(t,bound(j)/adx(j));
+%                     end
+                    t = min(t,bound(j)/adx(j));
+                end
+            end
+        end
+        t = t*.999;
+        while T*(x(:,i)+t*Dx)'*Q*(x(:,i)+t*Dx) + T*c'*(x(:,i)+t*Dx) - sum(log(A*(x(:,i)+ t*Dx) - b)) > (f+alpha*t*grad'*Dx)
+            t = beta * t;
         end 
         %update
         x(:,i+1) = x(:,i) + t * Dx ;
+        if isnan(x(1,i+1)) || isinf(x(1,i+1))
+            disp(t)
+        end
     end 
 end 
 end
--- mainBarrier.m
+++ mainBarrier.m
@@ -1,22 +1,43 @@
 % Initialisation:
-A = [-rand(1)*5, 0.5; rand(1)*7, -2*rand(1); rand(1), rand(1); -1, -1];
-b = [1; 2; 3 ; 4];
-c = [1 ; 1]
-T = 1
+%A = [-rand(1)*5, 0.5; rand(1)*7, -2*rand(1); rand(1), rand(1); -1, -1];
+%b = [1; 2; 3 ; 4];
+%c = [1 ; 1]
+%T = 1
+
+Q = eye(2); %quadratic term
+c = [0;0]; %linear term
+A = eye(2) %constraint matrix
+b = [0.5;-0.5];
+x0 = [1;1]
+T = 1;
+
+fun = @(x) x'*Q*x + c'*x;
+Aeq = zeros(1,size(Q,2));
+beq = 0;
+lb = [];
+ub = [];
+nonlcon = [];
+options = optimset('Display', 'off','Algorithm','interior-point');
+result_ip = fmincon(fun,x0,-A,-b,Aeq,beq,lb,ub,nonlcon,options);
+
 
 
 % Newton's method parameters
-maxIter = 150 ;
+%maxIter = 150 ;
+maxIter = 150;
 tol = 1e-10 ;
-alpha = 0.1 ;
+%tol = 0.15;
+%alpha = 0.1 ;
+alpha = 0.1;
 beta = 0.5 ;
 
 
 %% Interior points parameters
 thresholdIP = 1e-5 %threshold for interior point method
-mu = 1.5
-x0 = [0, 0]'
-T = 1
+%mu = 1.5
+mu = 1.5;
+%x0 = [1, 1]' %initial guess
+T = 1;
 dualityGap = zeros(1, maxIter) ; %track duality gap
 T_values = zeros(1, maxIter) ; %track values of T parameter
 N_Iterations = [] ; %used for the plotting
@@ -27,24 +48,25 @@ ord = [] ; %used for the plotting
 %% Solve the program using Interior Point method with Newton + backtracking at each step
 for j = 1:maxIter
     T_values(1,j) = T ;
-    [x,i,lambda, f, dualf, dualArg] =Newton2(A, b, c, T, x0, maxIter, tol, alpha, beta) ;
+    [x,i,lambda, f, dualf, dualArg] =Newton2(Q, A, b, c, T, x0, maxIter, tol, alpha, beta) ;
     dualityGap(:,j) = f - dualf ;
     N_Iterations = [N_Iterations, (1:i) + prev_i] ; %for plotting
     prev_i = prev_i + i ; %for plotting
     ord = [ord, repmat(dualityGap(:,j), 1, i)] ; %for plotting
     if (dualityGap(:,j) < thresholdIP)
+        disp('Interior Point Threshold Achieved -- Exiting');
         break;
     else
         T = mu*T;
-    x0 = x(:,i);
+        x0 = x(:,i);
     end;
 end;
 
 
 %% Plot duality gap VS iterations number
-Figure1=figure(1);clf;
-set(Figure1,'defaulttextinterpreter','latex');
-stairs(N_Iterations, ord)
-xlabel('Iterations')
-ylabel('Duality Gap')
-title('Duality Gap depending on the number of iterations')
+% Figure1=figure(1);clf;
+% set(Figure1,'defaulttextinterpreter','latex');
+% stairs(N_Iterations, ord)
+% xlabel('Iterations')
+% ylabel('Duality Gap')
+% title('Duality Gap depending on the number of iterations')
